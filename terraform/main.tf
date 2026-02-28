9a478f8c3d142be5dd0332bfbf851323fd28cff2
eliappo <237402327+eliappo@users.noreply.github.com>
2026-03-01 00:48:47 +0100

    fix: adding another 1 gb ram to droplets for deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,7 @@ provider "digitalocean" {
 resource "digitalocean_droplet" "db" {
   name   = "minitwit-db"
   image  = "ubuntu-22-04-x64"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
   region = "fra1"
   ssh_keys = [var.ssh_key_id]
 }
@@ -20,7 +20,7 @@ resource "digitalocean_droplet" "db" {
 resource "digitalocean_droplet" "web" {
   name   = "minitwit-web"
   image  = "ubuntu-22-04-x64"
-  size   = "s-1vcpu-1gb"
+  size   = "s-1vcpu-2gb"
   region = "fra1"
   ssh_keys = [var.ssh_key_id]
 }
