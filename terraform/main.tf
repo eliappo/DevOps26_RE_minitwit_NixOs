5d285479d988587553c7f37954cdcc44a1bb99cc
eliappo <237402327+eliappo@users.noreply.github.com>
2026-03-01 00:29:09 +0100

    Fix: Give actions the public ip of db_droplet to ensure access

--- terraform/main.tf
+++ terraform/main.tf
@@ -29,6 +29,10 @@ output "db_private_ip" {
   value = digitalocean_droplet.db.ipv4_address_private
 }
 
+output "db_public_ip" {
+  value = digitalocean_droplet.db.ipv4_address
+}
+
 output "web_public_ip" {
   value = digitalocean_droplet.web.ipv4_address
 }
