640062edcde828bde05032b07814a3ba9e24964d
eliappo <237402327+eliappo@users.noreply.github.com>
2026-03-01 01:26:45 +0100

    Fix: hopefully final fix to disk

--- modules/disk.nix
+++ modules/disk.nix
@@ -1,22 +1,22 @@
 { ... }:
 {
   disko.devices = {
-    disk = {
-      main = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            boot = {
-              size = "1M";
-              type = "EF02";
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "ext4";
-              };
+    disk.main = {
+      type = "disk";
+      device = "/dev/vda";
+      content = {
+        type = "gpt";
+        partitions = {
+          boot = {
+            size = "1M";
+            type = "EF02";
+          };
+          root = {
+            size = "100%";
+            content = {
+              type = "filesystem";
+              format = "ext4";
+              mountpoint = "/";
             };
           };
         };
--- nix-hosts/db/hardware-configuration.nix
+++ nix-hosts/db/hardware-configuration.nix
@@ -1,12 +1,1 @@
-{ config, lib, pkgs, ... }:
-{
-  fileSystems."/" = {
-    device = "/dev/sda1";
-    fsType = "ext4";
-  };
-
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda";
-  };
-}
+{ ... }: { }
